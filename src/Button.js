cb64200c016ee1e06f1b066d8447034be94565b6
Christian Johnson <charcoaldust@mac.com>
2023-07-11 16:08:08 -0700

    added validation to select only 1 button function

--- src/Button.js
+++ src/Button.js
@@ -1,4 +1,5 @@
 import React from "react";
+import PropTypes from "prop-types";
 
 const Button = ({
   children,
@@ -10,8 +11,23 @@ const Button = ({
   outline,
   rounded,
 }) => {
-  // Component Logic
   return <button>{children}</button>;
+};
+
+Button.propTypes = {
+  isValidButtonType: ({ primary, secondary, success, warning, danger }) => {
+    const count =
+      Number(!!primary) +
+      Number(!!secondary) +
+      Number(!!success) +
+      Number(!!warning) +
+      Number(!!danger);
+    if (count > 1) {
+      return new Error(
+        `Invalid prop, you must assign only one of the following: primary, secondary, success, warning, danger.`
+      );
+    }
+  },
 };
 
 export default Button;
