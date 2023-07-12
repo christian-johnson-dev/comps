9227a520f1ff7b16bf4481d506e8b520fd9534fd
Christian Johnson <charcoaldust@mac.com>
2023-07-12 13:59:25 -0700

    added classnames

--- src/Button.js
+++ src/Button.js
@@ -1,5 +1,5 @@
 import React from "react";
-import PropTypes from "prop-types";
+import className from "classnames";
 
 const Button = ({
   children,
@@ -11,7 +11,21 @@ const Button = ({
   outline,
   rounded,
 }) => {
-  return <button>{children}</button>;
+  const classes = className("px-3 py-1.5 border mb-1", {
+    "border-blue-500 bg-blue-500 text-white": primary,
+    "border-black bg-black text-white": secondary,
+    "border-green-500 bg-green-500 text-white": success,
+    "border-yellow-500 bg-yellow-500 text-white": warning,
+    "border-red-500 bg-red-500 text-white": danger,
+    "rounded-full": rounded,
+    "bg-transparent": outline,
+    "text-blue-500": outline && primary,
+    "text-black": outline && secondary,
+    "text-green-500": outline && success,
+    "text-yellow-500": outline && warning,
+    "text-red-500": outline && danger,
+  });
+  return <button className={classes}>{children}</button>;
 };
 
 Button.propTypes = {
@@ -24,7 +38,7 @@ Button.propTypes = {
       Number(!!danger);
     if (count > 1) {
       return new Error(
-        `Invalid prop, you must assign only one of the following: primary, secondary, success, warning, danger.`
+        `You can assign only one of the following: primary, secondary, success, warning, danger.`
       );
     }
   },
